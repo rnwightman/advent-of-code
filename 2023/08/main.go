42a2838da35dd9a51147d9a9df4311760c7a448e
Ryan Wightman <ryan@thewightmans.ca>
2023-12-09 09:12:35 -0500

    solves part 1 of day 8

--- 2023/08/main.go
+++ 2023/08/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"slices"
 )
 
 type Directions string
@@ -17,11 +18,52 @@ type Node struct {
 
 type Map []Node
 
+func (m Map) IndexOfLabel(l string) int {
+	return slices.IndexFunc(m, func(n Node) bool {
+		return l == n.Label
+	})
+}
+
 func main() {
 	dirs, m := ParseInput(os.Stdin)
+	steps := Solve(m, dirs)
 
+	fmt.Fprintln(os.Stdout, steps)
+}
+
+func Solve(m Map, dirs Directions) int {
 	fmt.Fprintln(os.Stderr, "Directions", dirs)
 	fmt.Fprintf(os.Stderr, "Map contains %d nodes\n", len(m))
+
+	startIndex := m.IndexOfLabel("AAA")
+	exitIndex := m.IndexOfLabel("ZZZ")
+	if startIndex == -1 || exitIndex == -1 {
+		panic("Unable to locate start and/or exit nodes on map")
+	}
+	fmt.Fprintln(os.Stderr, "Start", startIndex, "Exit", exitIndex)
+
+	steps := 0
+	for i := startIndex; i != exitIndex; {
+		d := dirs[steps%len(dirs)]
+
+		cur := m[i]
+		var nextLabel string
+		switch d {
+		case 'L':
+			nextLabel = cur.Left
+		case 'R':
+			nextLabel = cur.Right
+		default:
+			panic("unexpected direction")
+		}
+
+		i = m.IndexOfLabel(nextLabel)
+		steps += 1
+
+		fmt.Fprintf(os.Stderr, "Step: %d goes from %s to %s\n", steps, cur.Label, nextLabel)
+	}
+
+	return steps
 }
 
 func ParseInput(f *os.File) (Directions, Map) {
