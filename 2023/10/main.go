697fcdfba1fbc4f7e2c63c5c1ae6db5c384bdd5d
Ryan Wightman <ryan@thewightmans.ca>
2023-12-10 16:12:56 -0500

    feat(d10p1): solved part 1 of day 10

--- 2023/10/main.go
+++ 2023/10/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"slices"
 )
 
 type TileType rune
@@ -19,9 +21,41 @@ const (
 	StartingPos    TileType = 'S'
 )
 
+func (t TileType) String() string {
+	return fmt.Sprintf("%c", t)
+}
+
 type Coordinate struct {
 	Row    int
 	Column int
+}
+
+func (c Coordinate) North() Coordinate {
+	return Coordinate{
+		Row:    c.Row - 1,
+		Column: c.Column,
+	}
+}
+
+func (c Coordinate) South() Coordinate {
+	return Coordinate{
+		Row:    c.Row + 1,
+		Column: c.Column,
+	}
+}
+
+func (c Coordinate) East() Coordinate {
+	return Coordinate{
+		Row:    c.Row,
+		Column: c.Column + 1,
+	}
+}
+
+func (c Coordinate) West() Coordinate {
+	return Coordinate{
+		Row:    c.Row,
+		Column: c.Column - 1,
+	}
 }
 
 type Tile struct {
@@ -33,48 +67,117 @@ func (t Tile) IsStartingTile() bool {
 	return t.Type == StartingPos
 }
 
+func (t Tile) AdjacentPositions() []Coordinate {
+	switch t.Type {
+	case VerticalPipe:
+		return []Coordinate{t.Position.North(), t.Position.South()}
+
+	case HorizontalPipe:
+		return []Coordinate{t.Position.East(), t.Position.West()}
+
+	case BendNorthEast:
+		return []Coordinate{t.Position.North(), t.Position.East()}
+	case BendNorthWest:
+		return []Coordinate{t.Position.North(), t.Position.West()}
+	case BendSouthEast:
+		return []Coordinate{t.Position.South(), t.Position.East()}
+	case BendSouthWest:
+		return []Coordinate{t.Position.South(), t.Position.West()}
+
+	case StartingPos:
+		return []Coordinate{
+			t.Position.North(),
+			t.Position.East(),
+			t.Position.South(),
+			t.Position.West(),
+		}
+
+	default:
+		return []Coordinate{}
+	}
+}
+
 type Board struct {
 	Tiles [][]Tile
 }
 
-func (b Board) StartingTile() (*Tile, bool) {
+func (b Board) StartingTile() (Tile, bool) {
 	for _, row := range b.Tiles {
-		for i := range row {
-			t := &row[i]
-
+		for _, t := range row {
 			if t.IsStartingTile() {
 				return t, true
 			}
 		}
 	}
 
-	return nil, false
+	return Tile{}, false
 }
 
-func (b Board) TileAt(c Coordinate) (*Tile, bool) {
+func (b Board) TileAt(c Coordinate) (Tile, bool) {
 	if c.Row < 0 || c.Row >= len(b.Tiles) {
-		return nil, false
+		return Tile{}, false
 	}
 
 	row := b.Tiles[c.Row]
 	if c.Column < 0 || c.Column >= len(row) {
-		return nil, false
+		return Tile{}, false
 	}
 
-	tile := &row[c.Column]
+	tile := row[c.Column]
 	return tile, true
 }
 
-func (t TileType) String() string {
-	return fmt.Sprintf("%c", t)
+func (b Board) TileConnectsTo(c Tile) []Tile {
+	tiles := make([]Tile, 0)
+
+	for _, p := range c.AdjacentPositions() {
+		t, ok := b.TileAt(p)
+		if !ok {
+			continue
+		}
+
+		mutuallyAdjacent := slices.Contains(t.AdjacentPositions(), c.Position)
+		if mutuallyAdjacent {
+			tiles = append(tiles, t)
+		}
+	}
+
+	return tiles
+}
+
+func (b Board) NextTile(p, c Tile) Tile {
+	for _, aPos := range c.AdjacentPositions() {
+		if aPos == p.Position {
+			continue
+		}
+
+		t, ok := b.TileAt(aPos)
+		if !ok {
+			panic("walked off the board")
+		}
+		return t
+	}
+
+	panic("unable to find nex tile")
+}
+
+func (b Board) Explore(p, c Tile) (int, bool) {
+	if c.Type == StartingPos {
+		return 1, true
+	}
+
+	n := b.NextTile(p, c)
+	s, ok := b.Explore(c, n)
+	if !ok {
+		return 0, false
+	}
+
+	return s + 1, true
 }
 
 func main() {
 	input := readInput(os.Stdin)
-	fmt.Fprintln(os.Stderr, "Input", input)
-
 	board := parseBoard(input)
-	fmt.Fprintln(os.Stderr, "Board", board)
 
 	start, found := board.StartingTile()
 	if !found {
@@ -82,6 +185,21 @@ func main() {
 	}
 	fmt.Fprintln(os.Stderr, "Starting Pos", start)
 
+	adjToStart := board.TileConnectsTo(start)
+	fmt.Fprintln(os.Stderr, "Adj to Start", adjToStart)
+	if len(adjToStart) != 2 {
+		panic("unexpected number of candidate loops")
+	}
+
+	steps, ok := board.Explore(start, adjToStart[0])
+	if !ok {
+		panic("unable to explore the loop")
+	}
+
+	fmt.Fprintln(os.Stderr, "Length", steps)
+
+	dist := steps / 2
+	fmt.Fprintln(os.Stdout, dist)
 }
 
 func parseBoard(inputTiles [][]TileType) Board {
