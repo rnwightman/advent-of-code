9a5d6580c8219735e80e445aa03dea6544b564fe
Ryan Wightman <ryan@thewightmans.ca>
2023-12-17 12:44:24 -0500

    sovles day 12 part 1

--- 2023/12/main.go
+++ 2023/12/main.go
@@ -25,13 +25,126 @@ type Record struct {
 	DamagedGroups []int64
 }
 
+func ToDamagedGroups(conds []Condition) []int64 {
+	groups := make([]int64, 0)
+
+	var count int64 = 0
+	for _, c := range conds {
+		if c == Damaged {
+			count += 1
+		} else if count > 0 {
+			groups = append(groups, count)
+			count = 0
+		}
+	}
+
+	if count > 0 {
+		groups = append(groups, count)
+	}
+
+	return groups
+}
+
+func (r Record) Matches(conds []Condition) bool {
+	groups := ToDamagedGroups(conds)
+	if len(groups) != len(r.DamagedGroups) {
+		return false
+	}
+
+	for i := 0; i < len(groups); i++ {
+		if groups[i] != r.DamagedGroups[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Expand(conds []Condition) [][]Condition {
+	if len(conds) == 0 {
+		return [][]Condition{}
+	}
+
+	expanded := make([][]Condition, 0)
+	prefix := make([]Condition, 0)
+	for i, c := range conds {
+		if c != Unknown {
+			prefix = append(prefix, c)
+			continue
+		}
+
+		remainder := conds[i+1:]
+		expRemainder := Expand(remainder)
+
+		if len(expRemainder) == 0 {
+			// good
+			expandedGood := make([]Condition, 0, len(conds))
+			expandedGood = append(expandedGood, prefix...)
+			expandedGood = append(expandedGood, Operational)
+
+			// bad
+			expandedBad := make([]Condition, 0, len(conds))
+			expandedBad = append(expandedBad, prefix...)
+			expandedBad = append(expandedBad, Damaged)
+
+			expanded = append(expanded, expandedGood)
+			expanded = append(expanded, expandedBad)
+		}
+
+		for _, rem := range expRemainder {
+			// good
+			expandedGood := make([]Condition, 0, len(conds))
+			expandedGood = append(expandedGood, prefix...)
+			expandedGood = append(expandedGood, Operational)
+			expandedGood = append(expandedGood, rem...)
+
+			// bad
+			expandedBad := make([]Condition, 0, len(conds))
+			expandedBad = append(expandedBad, prefix...)
+			expandedBad = append(expandedBad, Damaged)
+			expandedBad = append(expandedBad, rem...)
+
+			expanded = append(expanded, expandedGood)
+			expanded = append(expanded, expandedBad)
+		}
+		break
+	}
+
+	if len(expanded) == 0 {
+		expanded = append(expanded, prefix)
+	}
+
+	return expanded
+}
+
+func (r Record) PossibleArrangements() [][]Condition {
+	return Expand(r.Conditions)
+}
+
 func main() {
-	fmt.Fprintln(os.Stderr, "WIP: Day 12")
+	sum := 0
 
 	records := parseRecords(os.Stdin)
 	for _, r := range records {
 		fmt.Fprintln(os.Stderr, r)
+
+		arrangements := r.PossibleArrangements()
+		for _, a := range arrangements {
+			if len(a) != len(r.Conditions) {
+				panic("Arrangement has different length than record")
+			}
+			ok := r.Matches(a)
+
+			if ok {
+				fmt.Fprintln(os.Stderr, "\t", a)
+				sum += 1
+			}
+		}
+
+		fmt.Fprintln(os.Stderr)
 	}
+
+	fmt.Fprintln(os.Stdout, sum)
 }
 
 func parseRecords(f *os.File) []Record {
