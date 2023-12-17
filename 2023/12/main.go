408f57b1715aca0a1d214493675d260ddf766d1e
Ryan Wightman <ryan@thewightmans.ca>
2023-12-17 16:06:08 -0500

    fix: only return counts not solutions

--- 2023/12/main.go
+++ 2023/12/main.go
@@ -25,17 +25,19 @@ type Record struct {
 	DamagedGroups []int64
 }
 
-func Solutions(conds []Condition, damagedGroups []int64, damagedCount int64) ([][]Condition, bool) {
-	solutions := make([][]Condition, 0)
-
+func Solutions(conds []Condition, damagedGroups []int64, damagedCount int64) int64 {
 	// Evertying is done
 	if len(conds) == 0 && len(damagedGroups) == 0 && damagedCount == 0 {
-		return solutions, true
+		return 1
 	}
 
 	// No more input and one group of damaged
 	if len(conds) == 0 {
-		return solutions, len(damagedGroups) == 1 && damagedCount == damagedGroups[0]
+		if len(damagedGroups) == 1 && damagedCount == damagedGroups[0] {
+			return 1
+		} else {
+			return 0
+		}
 	}
 
 	var targetDamaged int64 = 0
@@ -45,37 +47,30 @@ func Solutions(conds []Condition, damagedGroups []int64, damagedCount int64) ([]
 
 	// Too many damaged no further solutions
 	if damagedCount > targetDamaged {
-		return solutions, false
+		return 0
 	}
 
 	c := conds[0]
 
 	if c == Unknown {
-		var altSols [][]Condition
 		altConds := make([]Condition, 0, len(conds))
 		altConds = append(altConds, conds...)
 
 		// Operational Reality
 		altConds[0] = Operational
-		altSols, ok := Solutions(altConds, damagedGroups, damagedCount)
-		if ok {
-			solutions = append(solutions, altSols...)
-		}
+		opCount := Solutions(altConds, damagedGroups, damagedCount)
 
 		// Damaged Reality
 		altConds[0] = Damaged
-		altSols, ok = Solutions(altConds, damagedGroups, damagedCount)
-		if ok {
-			solutions = append(solutions, altSols...)
-		}
+		altCount := Solutions(altConds, damagedGroups, damagedCount)
 
-		return solutions, len(solutions) > 0
+		return opCount + altCount
 	}
 
 	if c == Operational {
 		if damagedCount > 0 {
 			if damagedCount != targetDamaged {
-				return [][]Condition{}, false
+				return 0
 			} else {
 				damagedGroups = damagedGroups[1:]
 				damagedCount = 0
@@ -86,38 +81,15 @@ func Solutions(conds []Condition, damagedGroups []int64, damagedCount int64) ([]
 		damagedCount += 1
 	}
 
-	remSolutions, ok := Solutions(conds[1:], damagedGroups, damagedCount)
-	if !ok {
-		return solutions, false
-	}
-
-	for _, remSol := range remSolutions {
-		solution := make([]Condition, 0, len(conds))
-		solution = append(solution, c)
-		solution = append(solution, remSol...)
-
-		solutions = append(solutions, solution)
-	}
-
-	if len(solutions) == 0 {
-		solution := []Condition{c}
-		solutions = append(solutions, solution)
-	}
-
-	return solutions, true
+	return Solutions(conds[1:], damagedGroups, damagedCount)
 }
 
-func (r Record) Solutions() [][]Condition {
-	sols, ok := Solutions(r.Conditions, r.DamagedGroups, 0)
-	if !ok {
-		panic("unable to find solution")
-	}
-
-	return sols
+func (r Record) Solutions() int64 {
+	return Solutions(r.Conditions, r.DamagedGroups, 0)
 }
 
 func main() {
-	sum := 0
+	var sum int64 = 0
 
 	var numberOfCopies int8 = 1
 	if len(os.Args) > 1 {
@@ -130,17 +102,10 @@ func main() {
 
 	records := parseRecords(os.Stdin, numberOfCopies)
 	for i, r := range records {
-		fmt.Fprintln(os.Stderr, r)
 		solutions := r.Solutions()
 
-		fmt.Fprintf(os.Stdout, "Row %d/%d ->\t%d\n", i+1, len(records), len(solutions))
-
-		for _, sol := range solutions {
-			fmt.Fprintln(os.Stderr, "\t", sol)
-			sum += 1
-		}
-
-		fmt.Fprintln(os.Stderr)
+		fmt.Fprintf(os.Stdout, "Row %d/%d ->\t%d\n", i+1, len(records), solutions)
+		sum += solutions
 	}
 
 	fmt.Fprintln(os.Stdout, sum)
