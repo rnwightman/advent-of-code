d212601f9a6baedfb85dbbe7ea8eb5ea3f88fc80
Ryan Wightman <ryan@thewightmans.ca>
2023-12-18 07:31:34 -0500

    day13p1: parsing input

--- 2023/13/main.go
+++ 2023/13/main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
+type Pattern []string
+
 func main() {
-	fmt.Fprintln(os.Stderr, "WIP - Day 13")
+	patterns := parseInput(os.Stdin)
+	for i, p := range patterns {
+		fmt.Fprintln(os.Stderr, "Pattern", i+1)
+		for _, l := range p {
+			fmt.Fprintln(os.Stderr, l)
+		}
+		fmt.Fprintln(os.Stderr)
+	}
+}
+
+func parseInput(f *os.File) []Pattern {
+	patterns := make([]Pattern, 0)
+
+	var pattern Pattern
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			patterns = append(patterns, pattern)
+			pattern = Pattern{}
+			continue
+		}
+
+		pattern = append(pattern, line)
+	}
+
+	patterns = append(patterns, pattern)
+
+	return patterns
 }
